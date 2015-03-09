164939a9e222fc87a49a58a1b575059414cb325d
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-03-08 21:16:52 -0700

    Initial commit

---
+++ PDDesktop/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.Storage.Pickers;
+
+namespace PDDesktop
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+        }
+
+        private async void ChooseDataFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var filePicker = new FileOpenPicker();
+            filePicker.FileTypeFilter.Add(".csv");
+            filePicker.SuggestedStartLocation = PickerLocationId.Desktop;
+
+            var selectedFiles = await filePicker.PickSingleFileAsync();
+
+            if (selectedFiles != null) {
+                // Parse the file
+            }
+        }
+    }
+}
