882e9a1d49a7c1468c253b7dc96c9f2fdacb7641
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2015-03-09 21:14:56 -0700

    Histogram. Some bugs

--- PDDesktop/MainPage.xaml.cs
+++ PDDesktop/MainPage.xaml.cs
@@ -32,6 +32,12 @@ namespace PDDesktop
 
         private List<double> dAcc;
 
+        Dictionary<double, int> histogramX;
+        Dictionary<double, int> histogramY;
+        Dictionary<double, int> histogramZ;
+
+        Dictionary<double, int> histogram;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -45,9 +51,19 @@ namespace PDDesktop
             dAccZ = new List<double>();
 
             dAcc = new List<double>();
+
+            histogramX = new Dictionary<double, int>();
+            histogramY = new Dictionary<double, int>();
+            histogramZ = new Dictionary<double, int>();
+            histogram = new Dictionary<double, int>();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            HideGraphElements();   
+        }
+
+        private void HideGraphElements()
         {
             XLabel.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
             YLabel.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
@@ -61,6 +77,26 @@ namespace PDDesktop
             YAxisEnd.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
             WindowSizeSelector.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
             WindowSizeLabel.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            GraphSelector.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            GraphSelectorBox.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        private void ShowGraphElements()
+        {
+            XLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            YLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            AxisSelector.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            AxisLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            WindowSelector.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            WindowLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            XAxisStart.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            XAxisEnd.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            YAxisStart.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            YAxisEnd.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            WindowSizeSelector.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            WindowSizeLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            GraphSelector.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            GraphSelectorBox.Visibility = Windows.UI.Xaml.Visibility.Visible;
         }
 
         private async void ReadDataFile(StorageFile dataFile)
@@ -83,9 +119,17 @@ namespace PDDesktop
             }
 
             CalculateDelta();
-            DisplayData();
+            CalculateHistogram();
             CreateWindows();
-            DrawGraph();
+
+            if (GraphSelectorBox.SelectedIndex == 0)
+            {
+                DrawGraph();
+            }
+            else
+            {
+                DrawHistogram();
+            }
         }
 
         private void CalculateDelta()
@@ -101,13 +145,13 @@ namespace PDDesktop
             dAccY.Add(0);
             dAccZ.Add(0);
             dAcc.Add(0);
-            
+
             for (int i = 1; i < length; i++)
             {
                 double dx = Math.Abs(accX[i] - accX[i - 1]);
                 double dy = Math.Abs(accY[i] - accY[i - 1]);
                 double dz = Math.Abs(accZ[i] - accZ[i - 1]);
-                
+
                 dAccX.Add(dx);
                 dAccY.Add(dy);
                 dAccZ.Add(dz);
@@ -116,15 +160,57 @@ namespace PDDesktop
             }
         }
 
-        private void DisplayData()
+        private void CalculateHistogram()
         {
-            int length = accX.Count();
+            histogramX.Clear();
+            histogramY.Clear();
+            histogramZ.Clear();
+            histogram.Clear();
 
-            string content = "";
+            int length = dAccX.Count();
 
-            for (int i = 0; i < length && i < 40; i++)
+            for (int i = 0; i < length; i++)
             {
-                content += "dX:" + dAccX[i].ToString() + " dY:" + dAccY[i].ToString() + " dZ:" + dAccZ[i].ToString() + "\n";
+                double dx = double.Parse(dAccX[i].ToString("0.00"));
+                double dy = double.Parse(dAccY[i].ToString("0.00"));
+                double dz = double.Parse(dAccZ[i].ToString("0.00"));
+                double d = double.Parse(dAcc[i].ToString("0.00"));
+
+                if (histogramX.ContainsKey(dx))
+                {
+                    histogramX[dx]++;
+                }
+                else
+                {
+                    histogramX[dx] = 1;
+                }
+
+                if (histogramY.ContainsKey(dy))
+                {
+                    histogramY[dy]++;
+                }
+                else
+                {
+                    histogramY[dy] = 1;
+                }
+
+                if (histogramZ.ContainsKey(dz))
+                {
+                    histogramZ[dz]++;
+                }
+                else
+                {
+                    histogramZ[dz] = 1;
+                }
+
+                if (histogram.ContainsKey(d))
+                {
+                    histogram[d]++;
+                }
+                else
+                {
+                    histogram[d] = 1;
+                }
             }
         }
 
@@ -138,8 +224,8 @@ namespace PDDesktop
 
             for (int i = 0; i < numWindows; i++)
             {
-                int begin = i * windowSize;
-                int end = begin + windowSize;
+                int begin = i * windowSize / 10;
+                int end = begin + windowSize / 10;
                 WindowSelector.Items.Add(begin.ToString() + " - " + end.ToString());
             }
 
@@ -148,18 +234,7 @@ namespace PDDesktop
 
         private void DrawGraph()
         {
-            XLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            YLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            AxisSelector.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            AxisLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            WindowSelector.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            WindowLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            XAxisStart.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            XAxisEnd.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            YAxisStart.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            YAxisEnd.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            WindowSizeSelector.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            WindowSizeLabel.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            ShowGraphElements();
 
             int axis = AxisSelector.SelectedIndex;
 
@@ -274,6 +349,72 @@ namespace PDDesktop
             }
         }
 
+        private void DrawHistogram()
+        {
+            ShowGraphElements();
+
+            int axis = AxisSelector.SelectedIndex;
+
+            Dictionary<double, int> readings;
+
+            Color graphColor;
+
+            switch (axis)
+            {
+                case 0:
+                    readings = histogramX;
+                    graphColor = Colors.Red;
+                    break;
+                case 1:
+                    readings = histogramY;
+                    graphColor = Colors.Green;
+                    break;
+                case 2:
+                    readings = histogramZ;
+                    graphColor = Colors.Blue;
+                    break;
+                case 3:
+                    readings = histogram;
+                    graphColor = Colors.Yellow;
+                    break;
+                default:
+                    return;
+            }
+
+            double x1, x2, y1, y2;
+            y1 = GraphBox.Height;
+
+            double maxKey = readings.Keys.Max();
+            double maxValue = readings.Values.Max();
+
+            double yScale = GraphBox.Height / maxValue;
+            double xScale = GraphBox.Width / (maxKey);
+
+            GraphBox.Children.Clear();
+
+            XAxisStart.Text = "0";
+            YAxisStart.Text = "0";
+            XAxisEnd.Text = maxKey.ToString();
+            YAxisStart.Text = maxValue.ToString();
+
+            foreach (KeyValuePair<double, int> reading in readings)
+            {
+                x1 = reading.Key * xScale;
+                x2 = reading.Key * xScale;
+                y2 = reading.Value * yScale;
+
+                Line line = new Line();
+                line.Stroke = new SolidColorBrush(graphColor);
+
+                line.X1 = x1;
+                line.X2 = x2;
+                line.Y1 = y1;
+                line.Y2 = y2;
+
+                GraphBox.Children.Add(line);
+            }
+        }
+
         private async void ChooseDataFileBtn_Click(object sender, RoutedEventArgs e)
         {
             var filePicker = new FileOpenPicker();
@@ -292,7 +433,15 @@ namespace PDDesktop
         {
             try
             {
-                DrawGraph();
+                if (GraphSelectorBox.SelectedIndex == 0)
+                {
+                    DrawGraph();
+                }
+                else
+                {
+                    DrawHistogram();
+                }
+                
             }
             catch (Exception) { }
         }
@@ -301,7 +450,15 @@ namespace PDDesktop
         {
             try
             {
-                DrawGraph();
+                if (GraphSelectorBox.SelectedIndex == 0)
+                {
+                    DrawGraph();
+                }
+                else
+                {
+                    DrawHistogram();
+                }
+                
             }
             catch (Exception) { };
         }
@@ -311,7 +468,15 @@ namespace PDDesktop
             try
             {
                 CreateWindows();
-                DrawGraph();
+
+                if (GraphSelectorBox.SelectedIndex == 0)
+                {
+                    DrawGraph();
+                }
+                else
+                {
+                    DrawHistogram();
+                }
             }
             catch (Exception) { };
         }
